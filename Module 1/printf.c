5e2f10174e0eea4d138e6f1fe946d7f1d7c6fa13
Parixit Sutariya <flashbomberapp@gmail.com>
2023-03-15 10:00:46 +0530

    print Information using printf

---
+++ Module 1/printf.c
@@ -0,0 +1,11 @@
+#include <stdio.h>
+
+int main()
+{
+    printf("Name : Parixit \n");
+    printf("Birthdate : 28-07-1998 \n");
+    printf("Age : 25 \n");
+    printf("Address : Surat \n");
+
+    return 0;
+}
