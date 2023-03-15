b31e2491f0772a4931746625df08ab2df130e4e5
Parixit Sutariya <flashbomberapp@gmail.com>
2023-03-15 10:46:16 +0530

    Write a program to make a square & cube of number

---
+++ Module 2/cube.c
@@ -0,0 +1,18 @@
+#include <stdio.h>
+
+int main()
+{
+
+    int n, cube;
+
+    printf("Enter Number : ");
+    scanf ("%d", &n);
+
+    cube = n * n * n;
+
+    printf ("Cube : %d", cube);
+    return 0;
+
+
+
+}
---
+++ Module 2/square.c
@@ -0,0 +1,13 @@
+#include <stdio.h>
+
+int main()
+{
+    int n, square;
+    printf ("Enter Number : ");
+    scanf ("%d", &n);
+
+    square = n * n;
+
+    printf ("Square : %d", square);
+    return 0;
+}
