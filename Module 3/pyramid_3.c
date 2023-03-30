d337aed95107d86a527fb73fc1fd1271a8a9c3b1
Parixit Sutariya <flashbomberapp@gmail.com>
2023-03-30 11:15:38 +0530

    Pyramid Programs

---
+++ Module 3/pyramid_3.c
@@ -0,0 +1,31 @@
+#include <stdio.h>
+
+/*
+
+* * * *
+  * * *
+    * *
+      *
+     
+*/
+
+void main()
+{
+
+    for (int i=1; i<=4; i++)
+    {
+        for (int j=1; j<=4; j++)
+        {
+            if (j < i)
+            {
+                printf (" ");
+            }
+            else
+            {
+                printf ("*");
+            }
+        }
+
+        printf ("\n");
+    }
+}
